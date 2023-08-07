a741e3f95f356651880b2c1a577794e2cc5a1424
Sush-1207 <141652644+Sush-1207@users.noreply.github.com>
2023-08-07 20:13:24 +0530

    Upload
    Portfolio

---
+++ Portfolio/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const menu = document.getElementById("menu");
+    const header = document.getElementsByTagName("header")[0];
+    const top = document.querySelector(".top");
+  
+    menu.addEventListener("click", function() {
+      this.classList.toggle("fa-times");
+      header.classList.toggle("toggle");
+    });
+  
+    window.addEventListener("scroll", function() {
+      menu.classList.remove("fa-times");
+      header.classList.remove("toggle");
+  
+      if (window.scrollY > 0) {
+        top.style.display = "block";
+      } else {
+        top.style.display = "none";
+      }
+    });
+    window.addEventListener("load", function() {
+      menu.classList.remove("fa-times");
+      header.classList.remove("toggle");
+  
+      if (window.scrollY > 0) {
+        top.style.display = "block";
+      } else {
+        top.style.display = "none";
+      }
+    });
+  
+  
+  });
+  
